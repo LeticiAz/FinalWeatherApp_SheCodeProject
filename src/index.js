d1786a510cf5a73df1ed4fb988ce2d5bf07e4bfb
LeticiAz <leticia.ig.azevedo@gmail.com>
2025-06-30 22:50:52 +0100

    Add js to update all info

--- src/index.js
+++ src/index.js
@@ -5,8 +5,44 @@ function updateWeatherInfo (response) {
 
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = response.data.city;
+
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML = response.data.condition.description;
+
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = response.data.temperature.humidity;
+
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML = response.data.wind.speed;
+
+    let timeElement = document.querySelector("#dateHour");
+    let date = new Date(response.data.time * 1000);
+    timeElement.innerHTML = formatDate(date);
+    
+    let iconElement = document.querySelector("#weather-icon");
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
 
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
 
 function searchCity (city){
     let apiKey = "c56dcb7t88aa43c2b2f5cfa690a8917o";
@@ -25,4 +61,6 @@ function handleSearchSubmit (event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity ("Aveiro");
+window.onload = function () {
+  searchCity("Aveiro");
+};
