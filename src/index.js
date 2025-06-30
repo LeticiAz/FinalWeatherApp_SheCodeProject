1ce8bb2bcb630125ea9a6de0b7d9cef2ba57d9ee
LeticiAz <leticia.ig.azevedo@gmail.com>
2025-06-30 22:18:32 +0100

    Add function to search city and update temp

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
+function updateWeatherInfo (response) {
+    let temp = Math.round(response.data.temperature.current);
+    let tempElement = document.querySelector("#temperature");
+    tempElement.innerHTML = temp;
+
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+}
+
+
+function searchCity (city){
+    let apiKey = "c56dcb7t88aa43c2b2f5cfa690a8917o";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
+    axios.get(apiUrl).then(updateWeatherInfo);
+}
+
+
 function handleSearchSubmit (event) {
     event.preventDefault();
     let city = document.querySelector("#search-form-input").value;
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = city;
+    searchCity(city);
 }
 
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity ("Aveiro");
