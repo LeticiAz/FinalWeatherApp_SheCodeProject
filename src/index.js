bfdb8588afe2c2dca5ac8c8ba223d559d6d9f557
LeticiAz <leticia.ig.azevedo@gmail.com>
2025-07-13 15:20:21 +0100

    Add API integration for Forecast

--- src/index.js
+++ src/index.js
@@ -63,6 +63,12 @@ function handleSearchSubmit (event) {
     searchCity(city);
 }
 
+function formatDay (timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ['Mon', 'Tue', 'Wen', 'Thu', 'Fri', 'Sat', 'Sun'];
+    return days[date.getDay()];
+}
+
 function getForecast(city){
     let apiKey = "c56dcb7t88aa43c2b2f5cfa690a8917o";
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`
@@ -72,23 +78,25 @@ function getForecast(city){
 
 function displayForecast (response) {
     console.log(response.data);
-
-    let days = ['Mon', 'Tue', 'Wen', 'Thu', 'Fri', 'Sat', 'Sun'];
+    let dayWeek = response.data.daily;
     let forcastHtml = "";
 
-    days.forEach(function (day) {
-        forcastHtml += `
-            <div class="weather-forecast-day">
-                <div class="weather-forecast-date">${day}</div>
-                <div class="weather-forecast-icon"></div>
-                <div class="weather-forecast-temps">
-                    <span class="weather-forecast-temp">
-                        <strong>12º</strong>
-                    </span>
-                    <span class="weather-forecast-temp">9º</span>
+    dayWeek.forEach(function (day, index) {
+        if (index<5) {
+            forcastHtml += `
+                <div class="weather-forecast-day">
+                    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+                    <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+                    <div class="weather-forecast-temps">
+                        <span class="weather-forecast-temp">
+                            <strong>${Math.round(day.temperature.maximum)}º</strong>
+                        </span>
+                        <span class="weather-forecast-temp">${Math.round(day.temperature.minimum)}º</span>
+                    </div>
                 </div>
-            </div>
-        `;
+            `;
+        }
+        
     });
     let forcastElement = document.querySelector("#forecast");
     forcastElement.innerHTML = forcastHtml;
